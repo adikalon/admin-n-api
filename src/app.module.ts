2c950140b21a672dc9fca27fc320e473275e9a4c
adikalon <webadev@yandex.ru>
2020-11-26 23:15:59 +0300

    Добавлен TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminModule } from '@admin-bro/nestjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    /**
+     * TypeORM
+     */
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+
+    /**
+     * AdminBro
+     */
     AdminModule.createAdmin({
       adminBroOptions: {
         rootPath: '/admin',
