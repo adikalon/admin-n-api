0047eb7f642651f4608964d98a644963decdc538
adikalon <webadev@yandex.ru>
2021-02-09 20:11:19 +0300

    Payload в фильтре ошибок

--- src/common/filters/default-exception.filter.ts
+++ src/common/filters/default-exception.filter.ts
@@ -6,42 +6,55 @@ import {
   Logger,
 } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import string from '../strings/exception';
 
 @Catch()
 export class DefaultExceptionFilter extends BaseExceptionFilter {
   private readonly logger = new Logger('Error');
 
-  catch(exception: Error | HttpException, host: ArgumentsHost) {
+  catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
     const response = ctx.getResponse<Response>();
-    let error = string.unknown;
+
     let status = 520;
+    let title = string.unknown;
+    const url = request.url;
+    const headers = JSON.stringify(request.headers, null, 2);
+    const body = JSON.stringify(request.body, null, 2);
+    let error = '';
+    let stack = '';
 
     if (exception instanceof HttpException) {
       status = exception.getStatus();
+      title = exception.message;
+      stack = exception.stack.replace(/ {4}/g, '  ');
       let res = exception.getResponse();
 
       if (typeof res === 'object') {
         res = JSON.stringify(res, null, 2);
       }
 
-      error = `${exception.message} [${exception.getStatus()}]\n${res}\n\n${
-        exception.stack
-      }`;
+      error = res;
     } else if (exception instanceof Error) {
       status = HttpStatus.INTERNAL_SERVER_ERROR;
+      title = exception.name;
+      stack = exception.stack.replace(/ {4}/g, '  ');
       let res = exception.message;
 
       if (typeof res === 'object') {
         res = JSON.stringify(res, null, 2);
       }
 
-      error = `${exception.name}\n${res}\n\n${exception.stack}`;
+      error = res;
+    } else {
+      error = JSON.stringify(exception, null, 2);
     }
 
-    this.logger.error(error);
+    const message = `${title} [${status}]\n\n[URL]:\n${url}\n\n[HEADERS]:\n${headers}\n\n[BODY]:\n${body}\n\n[ERROR]:\n${error}\n\n[STACK]:\n${stack}\n\n`;
+
+    this.logger.error(message);
 
     response.status(status).json({
       succsess: false,
